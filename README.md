aec25aa3cc07f3a21cc104ea539ce9d8eaa05ebe
rsabaric <sabaricr@seattleu.edu>
2018-10-14 17:05:22 -0700

    added stretch goals

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
 
 > A Vue.js project for Watts 4000 Week 2
 
+Experimenting with Vue elements
+
 ## Development Environment
 
 Requires basic [Node.js](https://nodejs.org/en/) installation and [Git](https://desktop.github.com/)
@@ -9,8 +11,6 @@ Requires basic [Node.js](https://nodejs.org/en/) installation and [Git](https://
 ## Build Setup
 
 ``` bash
-# install dependencies
-npm install
 
 # serve with hot reload at localhost:8080
 npm run dev
@@ -20,6 +20,14 @@ npm run build
 
 # build for production and view the bundle analyzer report
 npm run build --report
+```
+
+## deploy site via GitHub
+
+Alter config /index.js to change dist to docs.
+```
+    index: path.resolve(__dirname, '../docs/index.html'),
+    assetsRoot: path.resolve(__dirname, '../docs'),
 ```
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,32 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-      <p>What is {{ num1 }} times {{ num2 }}? <span v-if="product">{{ product }}</span></p>
-      <button v-on:click="calculateProduct">Calculate</button>
+      <p>Enter Number:
+      <input v-model.number.lazy="num3" placeholder="first number"></p>
+      <p>Enter Number:
+      <input v-model.number.lazy="num4" placeholder="second number"></p>
+      <p>Select Operator:
+      <select v-model="selected">
+       <option disabled value="">Please select one</option>
+       <option>Plus</option>
+       <option>Minus</option>
+       <option>Multiplied By</option>
+       <option>Divided By</option>
+      </select></p?>
+      <button v-on:click="testFunc">Calculate</button>
+      <p v-if="testval"> {{ num1}} {{selected}} {{num2}} = {{testval}}</p>
     <h2>2 Things that are difficult in JavaScript</h2>
     <ol>
       <li>naming things</li>
       <li>recursion</li>
       <li>off-by-one errors</li>
     </ol>
+    <h3>{{ msg2}}</h3>
+    <button v-on:click="yesCounter">Yes</button>
+    <button v-on:click="noCounter">No</button>
+    <p>Total number of yes votes = {{yes}}</p>
+    <p>Total number of no votes = {{ no }}</p>
+    <button v-on:click="resetVote">Reset Poll</button>
   </div>
 </template>
 
@@ -18,15 +36,44 @@ export default {
   data () {
     return {
       msg: 'I like warm cookies and ice cream.',
-      num1: 10,
-      num2: 50,
-      product: null
+      msg2: 'Quick Poll: Do you think this joke is funny?',
+      num1: null,
+      num2: null,
+      num3: null,
+      num4: null,
+      yes: 0,
+      no:0,
+      product: null,
+      testval: null
     }
   },
   methods: {
-    calculateProduct: function(){
-      this.product = this.num1 * this.num2;
-    }
+      yesCounter: function(){
+      this.yes = this.yes +1;
+          },
+    noCounter: function(){
+      this.no = this.no +1;
+          },
+    testFunc: function(){
+        if (this.selected=="Minus"){
+        this.testval = this.num3 - this.num4
+        }
+         if (this.selected=="Plus"){
+        this.testval = this.num3 + this.num4
+        }
+         if (this.selected=="Multiplied By"){
+        this.testval = this.num3 * this.num4
+        }
+         if (this.selected=="Divided By"){
+        this.testval = this.num3 / this.num4
+        }
+        this.num1 = this.num3;
+        this.num2 = this.num4;
+    },
+        resetVote: function(){
+      this.yes = 0;
+      this.no = 0;
+          }
   }
 }
 </script>
@@ -52,3 +99,4 @@ a {
   color: #42b983;
 }
 </style>
+
